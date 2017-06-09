cb9cf34a09372ad8d567b1f94860b7ac4d4add09
ppoohh <ppoohh@www.com>
2017-06-08 17:17:06 -0700

    bug

---
+++ app/src/main/java/com/example/peter/myapplication/Test.java
@@ -0,0 +1,11 @@
+package com.example.peter.myapplication;
+
+import com.example.peter.*;
+import com.google.android.gms.ads.*;
+
+public class Test {
+
+	public void foo() {
+		int val = R.color.colorPrimary;
+	}
+}
